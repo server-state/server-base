90db47067df247fe9e09db5cb85378fa718f8bb0
Ludwig Richter <richter@fliegwerk.com>
2019-09-04 15:00:48 +0200

    Added js-doc comments

--- index.js
+++ index.js
@@ -2,13 +2,30 @@ const express = require('express');
 const nodeModuleName = require('./src/module-name');
 const defaultPort = 8080;
 
+/**
+ * @module '@server-state/server-base'
+ */
+
+/** 
+ * @class ServerStateBase
+* An instance of a HTTP/S server listening on a specified port with an API endopoint
+*/
 module.exports = class ServerStateBase {
+	/**
+	 * Create a new instance of `ServerStateBase` that will, upon calling `listen()`, listen on the specified `port` (default `8080`)
+	 * 
+	 * @param {number} [port=8080] The port on which the server will listen
+	 */
 	constructor(port = defaultPort) {
 		this.port = port;
 		this.modules = [];
 		this.app = express();
 	}
 
+	/**
+	 * Add module to server for json output.
+	 * @param {string} name Module name (without prefixes)
+	 */
 	addModule(name) {
 		try {
 			require.resolve(nodeModuleName(name));
@@ -18,6 +35,9 @@ module.exports = class ServerStateBase {
 		}
 	}
 
+	/**
+	 * Begin listening on port specified in constructor
+	 */
 	start() {
 		// Apply modules
 		for (let module of this.modules) {
@@ -26,6 +46,7 @@ module.exports = class ServerStateBase {
 			});
 		}
 
+		// Pull requests from all modules and send them
 		this.app.get('/api/v1/all', (req, res) => {
 			let result = {};
 
@@ -35,6 +56,8 @@ module.exports = class ServerStateBase {
 
 			res.json(result);
 		});
+
+		// start web server
 		this.app.listen(this.port);
 	}
 };
--- src/module-name.js
+++ src/module-name.js
@@ -1,3 +1,15 @@
+/**
+ * Convert short-based module name to full-specificed module name, 
+ * e.g., `[module]` => `@server-state/[module]-module`.
+ * 
+ * If module name begins with "c-", custom module import are used, 
+ * e.g. `c-[module]` => `[module]`.
+ * @param {string} moduleName shortname module name
+ * @returns {string} Node module name, as requirable
+ */
 module.exports = function nodeModuleName(moduleName) {
+	if (moduleName.startsWith('c-')) {
+		return moduleName.substr(2, moduleName.length - 2);
+	}
 	return `@server-state/${moduleName}-module`;
 };
