d5576d5c0cac34a99ce91ab76c41a0b0506426dc
Pablo Klaschka <contact@pabloklaschka.de>
2019-09-05 12:19:31 +0200

    Added option to add options for modules

--- src/index.js
+++ src/index.js
@@ -12,18 +12,22 @@ module.exports = class ServerStateBase {
 	 */
 	constructor() {
 		this.modules = {};
+    this.args = {};
 	}
 
 	/**
 	 * Add module to server for json output.
 	 * @param {string} name Module name (without prefixes)
 	 * @param {function} fn
+   * @param {*} [options=undefined]
 	 */
-	addModule(name, fn) {
+	addModule(name, fn, options) {
 		if (this.modules[name])
 			console.error(`Module already used: ${name}. Skipping`);
-		else
+		else {
 			this.modules[name] = fn;
+      this.args[name] = args;
+    }
 	}
 
 	/**
@@ -35,7 +39,7 @@ module.exports = class ServerStateBase {
 		for (let module in this.modules) {
 			if (Object.prototype.hasOwnProperty.call(this.modules, module))
 				app.get('/api/v1/' + module, async (req, res) => {
-					return res.json(await this.modules[module]());
+					return res.json(await this.modules[module](this.args[module]));
 				});
 		}
 
@@ -45,7 +49,7 @@ module.exports = class ServerStateBase {
 
 			for (let module in this.modules) {
 				if (Object.prototype.hasOwnProperty.call(this.modules, module))
-					result[module] = await this.modules[module]();
+					result[module] = await this.modules[module](this.args[module]);
 			}
 
 			return res.json(result);
