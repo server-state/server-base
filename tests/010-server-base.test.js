6d541e1bd98df7b6c57e045ba9082945c20c195f
Pablo Klaschka <contact@pabloklaschka.de>
2019-09-06 11:19:44 +0200

    ESLint use spaces

--- tests/010-server-base.test.js
+++ tests/010-server-base.test.js
@@ -1,113 +1,113 @@
 const ServerState = require('../src/index');
 
 describe('server state class', () => {
-	it('should instantiate', () => {
-		expect(new ServerState()).toBeTruthy();
-	});
+    it('should instantiate', () => {
+        expect(new ServerState()).toBeTruthy();
+    });
 
-	describe('ServerState::init', () => {
-		/**
+    describe('ServerState::init', () => {
+        /**
          * @type {ServerState}
          */
-		let s;
+        let s;
 
-		let expressAppMock;
+        let expressAppMock;
 
-		beforeEach(() => {
-			s = new ServerState();
-			s.addModule('a', () => 'a');
-			s.addModule('b', (b) => b, 'b');
-			s.addModule('c', () => new Promise(resolve =>
-				setTimeout(() => resolve('c'), 100)
-			));
+        beforeEach(() => {
+            s = new ServerState();
+            s.addModule('a', () => 'a');
+            s.addModule('b', (b) => b, 'b');
+            s.addModule('c', () => new Promise(resolve =>
+                setTimeout(() => resolve('c'), 100)
+            ));
 
-			expressAppMock = {
-				get: jest.fn()
-			};
-		});
+            expressAppMock = {
+                get: jest.fn()
+            };
+        });
 
-		it('should correctly create all routes', () => {
-			s.init(expressAppMock);
-			expect(expressAppMock.get).toHaveBeenCalledTimes(4);
-		});
+        it('should correctly create all routes', () => {
+            s.init(expressAppMock);
+            expect(expressAppMock.get).toHaveBeenCalledTimes(4);
+        });
 
-		describe('route `all`', () => {
-			let routes = {};
-			const req = {};
-			const res = {
-				json: JSON.stringify
-			};
+        describe('route `all`', () => {
+            let routes = {};
+            const req = {};
+            const res = {
+                json: JSON.stringify
+            };
 
-			beforeEach(() => {
-				routes = {};
-				expressAppMock = {
-					get: (route, cb) => {
-						routes[route] = cb;
-					},
-					_run: (route) => {
-						return routes[route](req, res);
-					}
-				};
+            beforeEach(() => {
+                routes = {};
+                expressAppMock = {
+                    get: (route, cb) => {
+                        routes[route] = cb;
+                    },
+                    _run: (route) => {
+                        return routes[route](req, res);
+                    }
+                };
 
-				s.init(expressAppMock);
-			});
+                s.init(expressAppMock);
+            });
 
-			it('should include responses of all modules in the `all` route', async (done) => {
-				expect(await expressAppMock._run('/api/v1/all')).toBeTruthy();
-				const result = JSON.parse(await expressAppMock._run('/api/v1/all'));
-				expect(result).toHaveProperty('a');
-				expect(result).toHaveProperty('b');
-				expect(result).toHaveProperty('c');
+            it('should include responses of all modules in the `all` route', async (done) => {
+                expect(await expressAppMock._run('/api/v1/all')).toBeTruthy();
+                const result = JSON.parse(await expressAppMock._run('/api/v1/all'));
+                expect(result).toHaveProperty('a');
+                expect(result).toHaveProperty('b');
+                expect(result).toHaveProperty('c');
 
-				expect(result.a).toBe('a');
-				expect(result.b).toBe('b');
-				expect(result.c).toBe('c');
-				done();
-			});
+                expect(result.a).toBe('a');
+                expect(result.b).toBe('b');
+                expect(result.c).toBe('c');
+                done();
+            });
 
-			it('should correctly setup route for simple synchronous module a', async (done) => {
-				const resA = JSON.parse(await expressAppMock._run('/api/v1/a'));
-				expect(resA).toBe('a');
-				done();
-			});
+            it('should correctly setup route for simple synchronous module a', async (done) => {
+                const resA = JSON.parse(await expressAppMock._run('/api/v1/a'));
+                expect(resA).toBe('a');
+                done();
+            });
 
-			it('should correctly setup route for asynchronous module c', async (done) => {
-				const resC = JSON.parse(await expressAppMock._run('/api/v1/c'));
-				expect(resC).toBe('c');
-				done();
-			});
+            it('should correctly setup route for asynchronous module c', async (done) => {
+                const resC = JSON.parse(await expressAppMock._run('/api/v1/c'));
+                expect(resC).toBe('c');
+                done();
+            });
 
-			describe('passing options to modules', () => {
-				it('should correctly pass options to the module if specified', async (done) => {
-					const resB = JSON.parse(await expressAppMock._run('/api/v1/b'));
-					expect(resB).toBe('b');
-					done();
-				});
-			});
-		});
+            describe('passing options to modules', () => {
+                it('should correctly pass options to the module if specified', async (done) => {
+                    const resB = JSON.parse(await expressAppMock._run('/api/v1/b'));
+                    expect(resB).toBe('b');
+                    done();
+                });
+            });
+        });
 
-	});
+    });
 
-	describe('adding a module', () => {
-		/**
+    describe('adding a module', () => {
+        /**
          * @type {ServerState}
          */
-		let s;
+        let s;
 
-		beforeEach(() => {
-			s = new ServerState();
-		});
+        beforeEach(() => {
+            s = new ServerState();
+        });
 
-		it('should successfully add a module', () => {
-			s.addModule('test', () => true);
-			expect(s.modules).toHaveProperty('test');
-		});
+        it('should successfully add a module', () => {
+            s.addModule('test', () => true);
+            expect(s.modules).toHaveProperty('test');
+        });
 
-		it('should fail to add a module a second time', () => {
-			s.addModule('test', () => true);
-			console.error = jest.fn();
-			s.addModule('test', () => true);
-			expect(console.error).toHaveBeenCalled();
-		});
-	});
+        it('should fail to add a module a second time', () => {
+            s.addModule('test', () => true);
+            console.error = jest.fn();
+            s.addModule('test', () => true);
+            expect(console.error).toHaveBeenCalled();
+        });
+    });
 });
